8ed97b713c7c128749f7ad5a92988cc9c83541e5
Daniel San Martin <daniel.sanmartin.trabajo@gmail.com>
2023-01-16 22:24:15 -0300

    Reviso codigo y corrijo archivo Docker

--- app/main.py
+++ app/main.py
@@ -10,27 +10,28 @@ def run():
   countries = list(map(lambda x: x['Country'], data))
   percentages = list(map(lambda x: x['World Population Percentage'], data))
   '''
+  print('OK iniciando ejecucion con pandas')
 
   # Con pandas creamos un data frame direcatamente con la funcion read_csv, muy parecida la que creamos con anterioridad.
   df = pd.read_csv('data.csv')
   # filtramos inmediatamente
-  df = df[df['Continent'] == 'Africa']
+  df = df[df['Continent'] == 'Asia']
   # Obtengo los paises
   countries = df['Country'].values
   percentages = df['World Population Percentage'].values
-
   charts.generate_pie_chart(countries, percentages)
+  
+  print('OK en medio de la ejecución')
+
   data = read_csv.read_csv('data.csv')
-
   country = input('Type Country => ')
-
   result = utils.population_by_country(data, country)
-
   if len(result) > 0:
     country = result[0]
     labels, values = utils.get_population(country)
     charts.generate_bar_chart(country['Country'], labels, values)
   
+  print('OK terminado!')
 
 if __name__ == '__main__':
   run()
